fc8463d9807e24d6a4563001e9a2d4ab791db585
SangWon7242 <gotkdqja@gmail.com>
2022-10-12 02:52:03 +0900

    세팅

---
+++ src/main/java/com/sbs/exam/sb_app_2022_10_14/SbApp20221014Application.java
@@ -0,0 +1,13 @@
+package com.sbs.exam.sb_app_2022_10_14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbApp20221014Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbApp20221014Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sbs/exam/sb_app_2022_10_14/SbApp20221014ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbs.exam.sb_app_2022_10_14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbApp20221014ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
