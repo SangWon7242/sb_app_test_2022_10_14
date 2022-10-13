cb6856d6218af32fb445e16686bc041381c4a419
SangWon7242 <gotkdqja@gmail.com>
2022-10-13 15:00:25 +0900

    count 조절 액션 구현

---
+++ src/main/java/com/sbs/exam/sb_app_2022_10_14/app/article/controller/UsrHomeController.java
@@ -0,0 +1,33 @@
+package com.sbs.exam.sb_app_2022_10_14.app.article.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class UsrHomeController {
+  int count;
+
+  public UsrHomeController() {
+    count = -1;
+  }
+
+  @RequestMapping("/usr/home/main")
+  @ResponseBody
+  public String showMain() {
+    return "안녕하세요.";
+  }
+
+  @RequestMapping("/usr/home/getCount")
+  @ResponseBody
+  public int getCount() {
+    return count;
+  }
+
+  @RequestMapping("/usr/home/doSetCount")
+  @ResponseBody
+  public String doSetCount(int count) {
+    this.count = count;
+    return "count의 값이 "+ this.count + "으로 초기화 되었습니다.";
+  }
+}
