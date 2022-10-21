d61271fc8048fbf710c33a57f7422de56f869202
SangWon7242 <gotkdqja@gmail.com>
2022-10-21 14:06:53 +0900

    DELETE, UPDATE, SELECT 관련 쿼리를 메서드와 연결

--- src/main/java/com/sbs/exam/sb_app_2022_10_14/app/article/repository/ArticleRepository.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_14/app/article/repository/ArticleRepository.java
@@ -1,8 +1,7 @@
 package com.sbs.exam.sb_app_2022_10_14.app.article.repository;
 
 import com.sbs.exam.sb_app_2022_10_14.app.article.vo.Article;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
@@ -15,12 +14,12 @@ public interface ArticleRepository {
   @Select("SELECT * FROM article WHERE id = #{id}")
   public Article getArticle(int id);
 
-  // DELETE FROM article WHERE id = ?
+  @Delete("DELETE FROM article WHERE id = #{id}")
   public void deleteArticle(int id);
 
-  // UPDATE article SET title = ?, `body` = ?, updateDate = NOW() WHERE id = ?
+  @Update("UPDATE article SET title = #{title}, `body` = #{body}, updateDate = NOW() WHERE id = #{id}")
   public void modifyArticle(int id, String title, String body);
 
-  // SELECT * FROM article ORDER BY id DESC;
+  @Select("SELECT * FROM article ORDER BY id DESC")
   public List<Article> getArticles();
 }
