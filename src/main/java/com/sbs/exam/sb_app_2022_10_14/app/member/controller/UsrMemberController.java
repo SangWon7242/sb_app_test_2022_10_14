096493b8ad0e30cbbfd3ba5922be1eae11b6d54e
SangWon7242 <gotkdqja@gmail.com>
2022-10-25 03:31:04 +0900

    회원가입을 처리하기 위해, UsrMemberController, MemberService 추가

---
+++ src/main/java/com/sbs/exam/sb_app_2022_10_14/app/member/controller/UsrMemberController.java
@@ -0,0 +1,24 @@
+package com.sbs.exam.sb_app_2022_10_14.app.member.controller;
+
+import com.sbs.exam.sb_app_2022_10_14.app.member.service.MemberService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class UsrMemberController {
+  private MemberService memberService;
+
+  public UsrMemberController(MemberService memberService) {
+    this.memberService = memberService;
+  }
+
+  @RequestMapping("/usr/member/doJoin")
+  @ResponseBody
+  public String doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email)  {
+    memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
+    return "성공";
+  }
+}
+
+
---
+++ src/main/java/com/sbs/exam/sb_app_2022_10_14/app/member/repository/MemberRepository.java
@@ -0,0 +1,6 @@
+package com.sbs.exam.sb_app_2022_10_14.app.member.repository;
+
+public class MemberRepository {
+  public void join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
+  }
+}
