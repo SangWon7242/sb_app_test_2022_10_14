7cf4c5d45ef8a2356e7d3e27546f4bf4a49547db
SangWon7242 <gotkdqja@gmail.com>
2022-11-01 01:34:30 +0900

    usr/article/getArticle 에 ResultData 적용

--- src/main/java/com/sbs/exam/sb_app_2022_10_14/app/controller/UsrArticleController.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_14/app/controller/UsrArticleController.java
@@ -1,7 +1,9 @@
 package com.sbs.exam.sb_app_2022_10_14.app.controller;
 
 import com.sbs.exam.sb_app_2022_10_14.app.service.ArticleService;
+import com.sbs.exam.sb_app_2022_10_14.app.util.Ut;
 import com.sbs.exam.sb_app_2022_10_14.app.vo.Article;
+import com.sbs.exam.sb_app_2022_10_14.app.vo.ResultData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,12 +35,13 @@ public class UsrArticleController {
 
   @RequestMapping("/usr/article/getArticle")
   @ResponseBody
-  public Object getArticle(int id) {
+  public ResultData getArticle(int id) {
     Article article = articleService.getArticle(id);
     if ( article == null ) {
-      return id + "번 게시물은 존재하지 않습니다.";
+      return ResultData.from("F-1", Ut.f("%d번 게시물이 존재하지 않습니다.", id));
     }
-    return article;
+
+    return ResultData.from("S-1", Ut.f("%d번 게시물입니다.", id), article);
   }
 
   @RequestMapping("/usr/article/doDelete")
