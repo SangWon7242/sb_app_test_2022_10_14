5918c7b338839b198856cd53c1fca0579a25a3f9
SangWon7242 <gotkdqja@gmail.com>
2022-11-10 15:05:58 +0900

    NeedLogin인터셉터가 여러 액션들을 대신해서 자격없는 요청들을 앞선에서 거름

--- src/main/java/com/sbs/exam/sb_app_2022_10_14/app/vo/Rq.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_14/app/vo/Rq.java
@@ -1,10 +1,13 @@
 package com.sbs.exam.sb_app_2022_10_14.app.vo;
 
+import com.sbs.exam.sb_app_2022_10_14.app.util.Ut;
 import lombok.Getter;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 public class Rq {
 
@@ -13,7 +16,12 @@ public class Rq {
   @Getter
   private boolean isLogined;
 
-  public Rq(HttpServletRequest req) {
+  private HttpServletRequest req;
+  private HttpServletResponse resp;
+  public Rq(HttpServletRequest req, HttpServletResponse resp) {
+    this.req = req;
+    this.resp = resp;
+
     HttpSession httpSession = req.getSession();
     boolean isLogined = false;
     int loginedMemberId = 0;
@@ -27,4 +35,29 @@ public class Rq {
     this.loginedMemberId = loginedMemberId;
   }
 
+  public void printHistoryBackJs(String msg) {
+    resp.setContentType("text/html; charset=UTF-8");
+
+    println("<script>");
+
+    if( !Ut.empty(msg) ) {
+      println("alert('" + msg + "');");
+    }
+
+    println("history.back();");
+
+    println("</script>");
+  }
+
+  public void print(String str) {
+    try {
+      resp.getWriter().append(str);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public void println(String str) {
+    print(str + "\n");
+  }
 }
