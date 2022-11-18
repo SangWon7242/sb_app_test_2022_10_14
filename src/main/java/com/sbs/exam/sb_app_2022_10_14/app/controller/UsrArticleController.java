b9ad4324c6ab40fff3895d9acf5784f14006c0ad
gotkdqja@gmail.com <gotkdqja@gmail.com>
2022-11-18 14:39:29 +0900

    게시물 리스트 page 파라미터 처리

--- src/main/java/com/sbs/exam/sb_app_2022_10_14/app/controller/UsrArticleController.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_14/app/controller/UsrArticleController.java
@@ -10,6 +10,7 @@ import com.sbs.exam.sb_app_2022_10_14.app.vo.Rq;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
@@ -56,7 +57,7 @@ public class UsrArticleController {
   }
 
   @RequestMapping("/usr/article/list")
-  public String showList(Model model, int boardId) {
+  public String showList(Model model, @RequestParam(defaultValue = "1") int boardId, @RequestParam(defaultValue = "1") int page) {
     Board board = boardService.getBoardById(boardId);
 
     if (board == null) {
@@ -64,7 +65,9 @@ public class UsrArticleController {
     }
 
     int articlesCount = articleService.getArticleCount(boardId);
-    List<Article> articles = articleService.getForPrintArticles(rq.getLoginedMemberId(), boardId);
+
+    int itemsCountInAPage = 10;
+    List<Article> articles = articleService.getForPrintArticles(rq.getLoginedMemberId(), boardId, itemsCountInAPage, page);
 
     model.addAttribute("board", board);
     model.addAttribute("articlesCount", articlesCount);
