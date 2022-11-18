e7a86d7bc139a2e4f58347f35eb60cbd74b63a16
gotkdqja@gmail.com <gotkdqja@gmail.com>
2022-11-18 14:15:14 +0900

    BeforeActionInterceptor 에서 Rq 객체를 좀 더 확실하게 생성되도록 유도

--- src/main/java/com/sbs/exam/sb_app_2022_10_14/app/controller/UsrHomeController.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_14/app/controller/UsrHomeController.java
@@ -10,11 +10,6 @@ public class UsrHomeController {
     return "usr/home/main";
   }
 
-  @RequestMapping("/jsp")
-  public String showJsp() {
-    return "test";
-  }
-
   @RequestMapping("/")
   public String showRoot() {
     return "redirect:/usr/home/main";
