053d48547dd28c6b15e23db5187cd074185cd6b2
SangWon7242 <gotkdqja@gmail.com>
2022-11-29 10:25:10 +0900

    각 게시물에서 사용자가 좋아요와 싫어요를 할 수 있는지 체크

--- src/main/java/com/sbs/exam/sb_app_2022_10_14/app/controller/UsrArticleController.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_14/app/controller/UsrArticleController.java
@@ -90,6 +90,10 @@ public class UsrArticleController {
 
     model.addAttribute("article", article);
 
+    boolean actorCanMakeReactionPoint = articleService.actorCanMakeReactionPoint(rq.getLoginedMemberId(), id);
+
+    model.addAttribute("actorCanMakeReactionPoint", actorCanMakeReactionPoint);
+
     return "usr/article/detail";
   }
 
